3a8e7dde6d20260b1f12b95796f2130912f197c6
unknown <jwade1327@gmail.com>
2020-08-20 23:03:38 -0500

    button error

--- db/store.js
+++ db/store.js
@@ -2,17 +2,19 @@ const fs = require('fs');
 
 const util = require('util');
 
-const uuidv1 = require('uuid');
+const uuid = require('uuid');
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
 class Store {
     read() {
-        return readFileAsync(path.join(__dirname, 'db/db.json'), 'utf8');
+        return readFileAsync('db/db.json', 'utf8');
+        //return readFileAsync(path.join(__dirname, 'db/db.json'), 'utf8');
     }
     write(note) {
-        return writeFileAsync(path.join(__dirname, 'db/db.json'), JSON.stringify(note));
+        return writeFileAsync('db/db.json', JSON.stringify(note));
+        //return writeFileAsync(path.join(__dirname, 'db/db.json'), JSON.stringify(note));
     }
     getNotes() {
         return this.read().then(notes => {
@@ -32,7 +34,7 @@ class Store {
             error('please complete fields');
         }
 
-        const newNote = { title, text, id: uuid() };
+        const newNote = { title, text, id: uuidv1() };
         return this.getNotes()
             .then(notes => [...notes, newNote])
             .then(updatedNotes => this.write(updatedNotes))
