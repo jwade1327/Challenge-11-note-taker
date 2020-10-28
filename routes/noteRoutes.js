6196fe4081eefbf626804e5a8b58afac6141e674
unknown <jwade1327@gmail.com>
2020-10-28 01:10:26 -0500

    updates

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,16 +1,27 @@
 const router = require('express').Router();
 const store = require('../db/store');
 
-router.get('/notes', (req, res) => {
+router.get('/notes', function(req, res) {
     store
         .getNotes()
         .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err));
 });
 
 router.post('/notes', (req, res) => {
     store
         .addNote(req.body)
         .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err));
 });
+
+// need to fix...
+// router.delete('/notes', function(req, res) {
+//     store
+//         .removeNote(req.body)
+//         .then(notes => res.json(notes))
+//         // .then(() => res.json({ ok: true }))
+//         .catch(err => res.status(500).json(err));
+// });
 
 module.exports = router;
